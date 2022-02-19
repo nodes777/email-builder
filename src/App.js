e9bed51bf5189badb96dfa5fc7effdd5dad91581
nodes777 <nodes777@gmail.com>
2022-02-20 09:08:23 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import testTemplateJson from "./test-template.json";
+import EmailEditor from "react-email-editor";
 
-function App() {
+export const App = (props) => {
+  const emailEditorRef = useRef(null);
+
+  const exportHtml = () => {
+    emailEditorRef.current.editor.exportHtml((data) => {
+      const { design, html } = data;
+      console.log("exportHtml", html);
+    });
+  };
+
+  const saveDesign = () => {
+    emailEditorRef.current.editor.saveDesign((design) => {
+      console.log("saveDesign", design);
+    });
+  };
+
+  const onLoad = () => {
+    // editor instance is created
+    // you can load your template here;
+    const templateJson = testTemplateJson;
+    emailEditorRef.current.editor.loadDesign(templateJson);
+  };
+
+  const onReady = () => {
+    // editor is ready
+    console.log("onReady");
+    console.log(emailEditorRef.current.editor);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={exportHtml}>Export HTML</button>
+        <button onClick={saveDesign}>Save Design</button>
+      </div>
+
+      <EmailEditor
+        ref={emailEditorRef}
+        onLoad={onLoad}
+        onReady={onReady}
+        minHeight={"97vh"}
+      />
     </div>
   );
-}
+};
 
-export default App;
+// render(<App />, document.getElementById("root"));
